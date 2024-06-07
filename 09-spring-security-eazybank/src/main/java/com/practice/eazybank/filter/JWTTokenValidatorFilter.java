8de1247b1a8579d9a7e57a96c070752e8703ae94
Iovane Chikovani <iovane.chikovani.1@btu.edu.ge>
2024-06-07 22:52:06 +0400

    Covered Section 9: Token based Authentication using JWT

--- 09-spring-security-eazybank/src/main/java/com/practice/eazybank/filter/JWTTokenValidatorFilter.java
+++ 09-spring-security-eazybank/src/main/java/com/practice/eazybank/filter/JWTTokenValidatorFilter.java
@@ -12,14 +12,12 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Set;
 
 public class JWTTokenValidatorFilter extends OncePerRequestFilter {
 	@Override

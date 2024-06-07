094a60e9ecdc215414b0977c600dd7251add9210
Iovane Chikovani <iovane.chikovani.1@btu.edu.ge>
2024-06-07 22:51:21 +0400

    Covered Section 8: Token based Authentication using JWT

---
+++ 09-spring-security-eazybank/src/main/java/com/practice/eazybank/constants/SecurityConstants.java
@@ -0,0 +1,7 @@
+package com.practice.eazybank.constants;
+
+public interface SecurityConstants {
+	public static final String JWT_KEY = "1dda37427613de044ef3774d12b9d890";
+	public static final String JWT_HEADER = "Authorization";
+
+}
